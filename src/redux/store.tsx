4b5bc028b377c188d9c73bf824a3bcbd4a09baef
Srineevasan <srinivasan.d.mail@gmail.com>
2022-08-12 09:20:29 +0530

    z

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,6 @@
 import {configureStore,applyMiddleware} from "@reduxjs/toolkit";
-//import reduxThunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-//const  middlewares = [reduxThunk];
-
-//const store = configureStore({reducer:rootReducer,},applyMiddleware(...middlewares));
 const store = configureStore({reducer:rootReducer})
 export type RootState = ReturnType<typeof store.getState>
 export default store;
